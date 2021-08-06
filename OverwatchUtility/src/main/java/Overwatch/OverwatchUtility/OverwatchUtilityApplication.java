b07cd56c941ccb9f5a7fe1590c067b1dbd6c1683
gitgub masatomomurata <johnuinchester@gmail.com>
2021-08-07 00:50:58 +0900

    新規プロジェクト作成 ベースとなるプロジェクトをSpringInitializerにて作成。

---
+++ OverwatchUtility/src/main/java/Overwatch/OverwatchUtility/OverwatchUtilityApplication.java
@@ -0,0 +1,13 @@
+package Overwatch.OverwatchUtility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverwatchUtilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OverwatchUtilityApplication.class, args);
+	}
+
+}
---
+++ OverwatchUtility/src/test/java/Overwatch/OverwatchUtility/OverwatchUtilityApplicationTests.java
@@ -0,0 +1,13 @@
+package Overwatch.OverwatchUtility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OverwatchUtilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
